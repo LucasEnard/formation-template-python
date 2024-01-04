2d1adbd3f43c18f62056174b096ad4a1d6b8c807
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-01-04 12:37:21 +0100

    SaveInTxtBo : self.logger.info --> self.log_info modified the steps of on_init + the sequences of on_formation_request tasks ReadCsvBs : /irisdev/app/data --> /irisdev/app/misc + self.log_info(f'FormationRequest {msg.id} sent to {self.target}')

--- src/hello_world/msg.py
+++
@@ -1,6 +0,0 @@
-from grongier.pex import Message
-from dataclasses import dataclass
-
-@dataclass
-class MyMsg(Message):
-    value: str = ''
--- src/training/msg.py
+++
@@ -1,8 +0,0 @@
-from grongier.pex import Message
-from dataclasses import dataclass
-
-@dataclass
-class FormationRequest(Message):
-    id: int = 0
-    nom: str = ''
-    salle: str = ''
